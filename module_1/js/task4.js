4ba53d7cf70b2894468c358f9c59579690c1bbc4
Rodion Mokan <mokan11r@gmail.com>
2020-04-21 13:46:40 +0300

    fix2_4_5

--- module_1/js/task4.js
+++ module_1/js/task4.js
@@ -3,17 +3,21 @@ const pricePerDroid = 3000;
 let message;
 
 let input = prompt('Введите необходимое кол-во дроидов');
-let totalPrice = input * pricePerDroid;
+let totalPrice;
 
 if (input === null) {
   message = 'Отменено пользователем';
   console.log(message);
-} else if (totalPrice > credits) {
-  console.log('Недостаточно средств на счету!');
 } else {
-  message = `Вы купили ${input} дроидов, на счету осталось ${
-    credits - totalPrice
-  } кредитов.`;
-  console.log(message);
-  alert(message);
+  totalPrice = input * pricePerDroid;
+
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    message = `Вы купили ${input} дроидов, на счету осталось ${
+      credits - totalPrice
+    } кредитов.`;
+    console.log(message);
+    alert(message);
+  }
 }
